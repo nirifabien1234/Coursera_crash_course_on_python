498fd2ede145e8e21ea33c9e78b00c22d4c06307
nirifabien1234 <nirifabien@gmail.com>
2021-07-08 18:52:42 +0200

    A method to return a formated string

--- Formating_strings.py
+++ Formating_strings.py
@@ -1,3 +1,10 @@
-name = "Fabien"
-number = len(name) * 3
-print("Hello {}, your lucky number is {}".format(name, number))
+# name = "Fabien"
+# number = len(name) * 3
+# print("Hello {}, your lucky number is {}".format(name, number))
+
+def student_grade(name, grade):
+    	return "{name} received {grade}% on the exam".format(name=name, grade=grade)
+
+print(student_grade("Reed", 80))
+print(student_grade("Paige", 92))
+print(student_grade("Jesse", 85))
